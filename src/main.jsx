5eac89beef1814c4d4a9cc0ae4524195f6a39120
ElodieAllegro <124352648+ElodieAllegro@users.noreply.github.com>
2024-11-27 18:37:17 +0100

    push4

--- src/main.jsx
+++ src/main.jsx
@@ -1,51 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Location from './pages/Location/Location'
-import Error404 from './pages/404/404.jsx'
-import Layout from './components/Layout/Layout'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Location from './pages/Location/Location';
+import Error404 from './pages/404/404.jsx';
+import Layout from './components/Layout/Layout';
+import './index.css';
 
 import {
   RouterProvider,
   Route,
-  createBrowserRouter,
+  createHashRouter,
   createRoutesFromElements,
 } from "react-router-dom";
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
-    <Route 
-      element={<Layout />}
-    >
-      <Route path="" element={<Home />} />
-      <Route 
+    <Route element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      <Route
         path="locations/:id"
-        element={<Location />} 
+        element={<Location />}
         errorElement={<Error404 />}
         loader={({ params }) => {
           return fetch('/data.json')
-            .then(res => res.json())
-            .then(data => {
-
-              const l = data.find(d => d.id === params.id)              
-              if(!l){
+            .then((res) => res.json())
+            .then((data) => {
+              const l = data.find((d) => d.id === params.id);
+              if (!l) {
                 throw new Response("Not Found", { status: 404 });
               }
-              return l
-
-          })
+              return l;
+            });
         }}
       />
       <Route path="about" element={<About />} />
       <Route path="*" element={<Error404 />} />
     </Route>
   )
-)
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
