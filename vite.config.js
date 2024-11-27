88342bb497c764b209a3103e7075d134c41a7b1e
ElodieAllegro <124352648+ElodieAllegro@users.noreply.github.com>
2024-11-27 18:47:19 +0100

    push5

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-const BASE_URL = process.env.BASE_URL || '';
-
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `${BASE_URL}/`,
-})
+  base: '/elodie-p11-main/', // Chemin de base pour GitHub Pages
+});
